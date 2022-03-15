dcf449c6c3ab36d5e3cae2f411c9fac4bc4f8f3a
Tuinuii <tuyhappy@gmail.com>
2022-03-15 23:29:03 +0700

    Update main.py

--- PyCharm_basic/main.py
+++ PyCharm_basic/main.py
@@ -5,4 +5,5 @@ print("Tom Yum Kung             x 1  : 150 Baht")
 print("Chicken Coconut Soup     x 1  : 100 Baht")
 print("Omlet                    x 2  : 70 Baht")
 print("Chicken Massaman         x 1  : 50 Baht")
+print("Coco                     x 1  : 50 Baht")
 
